3a2198dd2b6b15b99ce7e8dd4291281eaaa974ad
Tareq Mohammad Yousuf <tareq.y@gmail.com>
2018-08-06 12:36:38 +0600

    a sample multimodule spring boot hello world application.

---
+++ application/src/main/java/com/example/application/Application.java
@@ -0,0 +1,27 @@
+package com.example.application;
+
+import com.example.service.MyService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(scanBasePackages = "com.example")
+@RestController
+public class Application {
+
+    private final MyService myService;
+
+    public Application(MyService myService) {
+        this.myService = myService;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return myService.message();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
